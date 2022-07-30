2512b10892731ae07e8a3f242430c2fadd9feec2
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2022-07-30 18:55:50 +0530

    filter sidebar fixed

--- src/App/Pages/Products.js
+++ src/App/Pages/Products.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import Sidebar from "../Components/Admin/Sidebar";
 import ProductCard from "../Components/Common/ProductCard";
+import FilterSidebar from "../Components/Layout/FilterSidebar";
 const data = [
   {
     id: 1,
@@ -55,88 +57,28 @@ const gender = ["Male", "Female"];
 
 
 export default function Products() {
+
+  const [sidebar, setSidebar] = useState(window.innerWidth>444);
+
   return (
     <div className="w-100 bg-white min-h-screen relative">
       <div className="py-28 px-5 md:px-20 max-w-screen-xl mx-auto">
-        <div className="">
-          <p className="text-gray text-sm md:text-lg">
-            Home / Shoes / Sneakers
-          </p>
-          <h1 className="text-2xl md:text-4xl">Sneakers</h1>
-        </div>
-        <div className="flex flex-row flex-wrap md:mt-5">
-          <div className="hidden md:w-1/5 md:flex md:flex-col p-2">
-            <div className="w-100 pr-3 border-b-[1px] py-4">
-              <h2 className="text-xl font-medium mb-2">Sort</h2>
-              <div className="relative">
-                <select
-                  className="block appearance-none w-full bg-gray-100 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                  id="grid-state"
-                >
-                  <option>Latest</option>
-                  <option>Price Ascending</option>
-                  <option>Price Descending</option>
-                </select>
-                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                  <svg
-                    className="fill-current h-4 w-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                  </svg>
-                </div>
-              </div>
-            </div>
-            <div className="w-100 flex flex-col border-b-[1px] py-4">
-              <h2 className="text-xl font-medium mb-2">Filter by Brand</h2>
-              {brands.map((item) => (
-                <div
-                  key={item}
-                  className="flex flex-row align-middle h-100 items-center content-center mb-1"
-                >
-                  <input
-                    type="checkbox"
-                    id="Asics"
-                    className="mr-2 bg-black"
-                  ></input>
-                  <p className="mb-1">{item}</p>
-                </div>
-              ))}
-            </div>
-            <div className="w-100 flex flex-col border-b-[1px] py-4">
-              <h2 className="text-xl font-medium mb-2">Filter by Color</h2>
-              {colors.map((item) => (
-                <div
-                  key={item}
-                  className="flex flex-row align-middle h-100 items-center content-center mb-1"
-                >
-                  <input
-                    type="checkbox"
-                    id="Asics"
-                    className="mr-2 bg-black"
-                  ></input>
-                  <p className="mb-1">{item}</p>
-                </div>
-              ))}
-            </div>
-            <div className="w-100 flex flex-col border-b-[1px] py-4">
-              <h2 className="text-xl font-medium mb-2">Filter by Gender</h2>
-              {gender.map((item) => (
-                <div
-                  key={item}
-                  className="flex flex-row align-middle h-100 items-center content-center mb-1"
-                >
-                  <input
-                    type="checkbox"
-                    id="Asics"
-                    className="mr-2 bg-black"
-                  ></input>
-                  <p className="mb-1">{item}</p>
-                </div>
-              ))}
-            </div>
+        <div className="flex flex-row justify-between items-center">
+          <div>
+            <p className="text-gray text-sm md:text-lg">
+              Home / Shoes / Sneakers
+            </p>
+            <h1 className="text-2xl md:text-4xl">Sneakers</h1>
           </div>
+          <div>
+            <button className="md:hidden text-xl" onClick={()=>(setSidebar(!sidebar))}>
+              <i class="ri-equalizer-line"></i>
+            </button>
+          </div>
+        </div>
+
+        <div className="flex flex-row flex-wrap md:mt-5">
+          {sidebar && <FilterSidebar colors={colors} brands={brands} gender={gender} />}
           <div className="w-full md:w-4/5 flex flex-row flex-wrap">
             <div className="w-1/2 md:w-1/3">
               <ProductCard item={data[0]} cardw="w-full" imgH="h-auto" />
